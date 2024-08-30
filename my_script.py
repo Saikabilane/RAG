95a84dd0b2bed4cf0f13084ae8190cd9b52e5b15
Saikabilane <89825675+Saikabilane@users.noreply.github.com>
2024-08-30 22:51:45 +0530

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,7 +1,6 @@
 import google.generativeai as genai
 import chromadb
 chroma_client = chromadb.PersistentClient(path="./collection")
-chroma_client.delete_collection(name = "my_collection")
 from pypdf import PdfReader
 import pdfplumber
 import streamlit as st
@@ -9,7 +8,7 @@ import streamlit as st
 api_key = "AIzaSyBt41MC3ZSxYlBktQH0WN_OFP45Jz7zjYs"
 genai.configure(api_key = api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
-filePath = "D:\\Projects\\RAG\\anatomy_vol_3_edited_final.pdf"
+filePath = "./anatomy_vol_3_edited_final.pdf"
 
 reader = PdfReader(filePath)
 pages = len(reader.pages)
@@ -47,9 +46,5 @@ def ask(prompt):
     data = ask_query(prompt)
     response = model.generate_content(f"Using this data: {data}, answer to this prompt: {prompt}.")
     x = response.text
-    #st.write(x)
     return x
-
-print(ask(input()))
-
 #chroma_client.delete_collection(name="my_collection")
